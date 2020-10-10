207f9379276d82da85a970c432a5c18d4ac0d272
rivu <58026024+unphydra@users.noreply.github.com>
2020-10-10 16:57:29 +0530

    added feature of three state of a item

--- src/todo/todo.js
+++ src/todo/todo.js
@@ -3,9 +3,17 @@ import React from 'react';
 //each item -> {name: 'banana', id:<number> checked: False}
 //list -> [item1, item2]
 
-const Item = ({ text, onClick, checked, id }) => {
-  const color = checked ? 'mediumseagreen' : 'lightblue';
-  const decoration = checked ? 'line-through' : 'none';
+const itemState = {
+  notDone: { colour: 'mediumseagreen', decoration: 'none' },
+  doing: { colour: 'orange', decoration: 'none' },
+  done: { colour: 'lightblue', decoration: 'line-through' },
+};
+
+itemState.notDone.next = itemState.doing;
+itemState.doing.next = itemState.done;
+itemState.done.next = itemState.notDone;
+
+const Item = ({ text, onClick, todoState, id }) => {
   return (
     <div
       className="item"
@@ -15,13 +23,13 @@ const Item = ({ text, onClick, checked, id }) => {
       <div
         className="indicator"
         style={{
-          backgroundColor: color,
+          backgroundColor: todoState.colour,
         }}
       ></div>
       <div
         className="title"
         style={{
-          textDecorationLine: decoration,
+          textDecorationLine: todoState.decoration,
         }}
       >
         {text}
@@ -76,13 +84,17 @@ class Todo extends React.Component {
   handleClick(id) {
     const list = this.state.list.slice();
     const item = list.find((l) => l.id === +id);
-    item.checked = !item.checked;
+    item.todoState = item.todoState.next;
     this.setState(() => ({ list }));
   }
 
   handleInput(title) {
     const list = this.state.list.slice();
-    list.push({ name: title, checked: false, id: this.id++ });
+    list.push({
+      name: title,
+      todoState: itemState.notDone,
+      id: this.id++,
+    });
     this.setState(() => ({ list }));
   }
 
@@ -92,7 +104,7 @@ class Todo extends React.Component {
         text={item.name}
         key={item.id}
         id={item.id}
-        checked={item.checked}
+        todoState={item.todoState}
         onClick={this.handleClick}
       ></Item>
     ));
